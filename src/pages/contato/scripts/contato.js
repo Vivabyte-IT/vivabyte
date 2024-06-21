164ba349ab93e43686ae47b44c526fa8dc3d9c61
Moares Oliveira <moaresoliveira@gmail.com>
2024-06-21 14:34:42 -0300

    Contatos e Index

--- src/pages/contato/scripts/contato.js
+++ src/pages/contato/scripts/contato.js
@@ -1,9 +1,21 @@
+// import token from '../../../config'
+
 const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const formData = new FormData(form)
   for (const pair of formData.entries()) {
-    console.log(pair)
+    console.log(`${pair[0]}: ${pair[1]}`)
   }
-  alert(JSON.stringify(Object.fromEntries(formData)))
+  let objForm = Object.fromEntries(formData.entries())
+  console.log(objForm)
+  let mensagem = `Olá, gostaria de saber mais sobre ${objForm.interesse}.\n
+${objForm.mensagem}\n
+  \n
+Atenciosamente,\n
+${objForm.nome} ${objForm.sobrenome}\n
+${objForm.email} - ${objForm.telefone}\n
+  `
+  console.log(mensagem)
+  window.open(`mailto:vivabyte.ti@gmail.com?subject=${objForm.interesse}&body=${mensagem}`);
 })
